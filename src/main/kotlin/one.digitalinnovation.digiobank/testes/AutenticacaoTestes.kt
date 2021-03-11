57991bd898076f22f57c0f459ae47050813696ec
emanuelle04 <manurs902@gmail.com>
2021-03-11 17:20:53 -0300

    Teste de autenticação da função login().

---
+++ src/main/kotlin/one.digitalinnovation.digiobank/testes/AutenticacaoTestes.kt
@@ -0,0 +1,8 @@
+package one.digitalinnovation.digiobank.testes
+
+import one.digitalinnovation.digiobank.Logavel
+
+class AutenticacaoTestes {
+    fun autenticaLogin(logavel: Logavel)= println(logavel.login())
+
+}
