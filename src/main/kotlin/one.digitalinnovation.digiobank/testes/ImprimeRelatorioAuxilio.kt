180abad8a4f8debcb1e2d5daa491bce4b7cdbfdd
emanuelle04 <manurs902@gmail.com>
2021-03-11 17:22:42 -0300

    Implementação da função imprimeRelatório.

---
+++ src/main/kotlin/one.digitalinnovation.digiobank/testes/ImprimeRelatorioAuxilio.kt
@@ -0,0 +1,7 @@
+package one.digitalinnovation.digiobank.testes
+
+import one.digitalinnovation.digiobank.Funcionario
+
+fun imprimeRelatorio(funcionario: Funcionario){
+    println(funcionario.toString())
+}
